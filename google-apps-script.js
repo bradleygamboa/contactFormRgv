d46b10e9eabdb638a91f0449f426fa73f923c425
Bradley Gamboa <bradleygamboa@gmail.com>
2018-02-26 13:32:55 -0600

    trying to edit form to say where its being submitted from

--- google-apps-script.js
+++ google-apps-script.js
@@ -39,7 +39,7 @@ function doPost(e) {
 
     MailApp.sendEmail({
       to: String(sendEmailTo),
-      subject: "Contact form submitted for RGV",
+      subject: "RGV Contact Form",
       // replyTo: String(mailData.email), // This is optional and reliant on your form actually collecting a field named `email`
       htmlBody: formatMailBody(mailData, dataOrder)
     });
